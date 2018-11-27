c19178c89787e05080832fbf7abae1062ecde08a
Jefek <jeffrey.knowles@gmail.com>
2018-11-27 10:41:40 -0500

    coinChange

---
+++ coinChange.js
@@ -0,0 +1,35 @@
+//
+// Coin Change
+// Objectives:
+//
+//     Write a function that creates and returns an object.
+//
+// Given a number of US cents, return the optimal configuration (meaning the largest denominations possible) of coins in an object. Use dollars, quarters, dimes, nickels, and pennies.
+//
+// Example: coinChange(312) returns
+//
+// {dollars: 3, dimes: 1, pennies: 2}
+//
+// Example: coinChange(78) returns
+//
+// {quarters: 3, pennies: 3}
+//
+// BONUS: Given an object with certain amounts of denominations of US money, alter the object so that the configuration is optimal.
+//
+// Example: coinChange({dollars: 2, dimes: 15, pennies: 5}) returns
+//
+// {dollars: 3, quarters: 2, nickels: 1}
+//
+
+function coinChange(pennyCollection) {
+
+  coinReference = {dollar : 100, dime : 10, nickel : 5, penny : 1};
+  coinCollection = {};
+
+  for ( coin in coinReference ) {
+    coinCollection[coin] = Math.floor(pennyCollection / coinReference[coin]);
+    pennyCollection %= coinReference[coin];
+  }
+  return coinCollection;
+}
+console.log(coinChange(312));
